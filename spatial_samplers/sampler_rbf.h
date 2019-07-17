538410588b5d6da0ee7d9e1aa0ccfbe83e7a9c8f
Stephen Longshaw <stephen.longshaw@stfc.ac.uk>
2019-07-17 14:30:25 +0100

    Update RBF sampler to new coding style

--- spatial_samplers/sampler_rbf.h
+++ spatial_samplers/sampler_rbf.h
@@ -1,27 +1,49 @@
-/*
-Multiscale Universal Interface Code Coupling Library
+/*****************************************************************************
+* Multiscale Universal Interface Code Coupling Library                       *
+*                                                                            *
+* Copyright (C) 2019 Y. H. Tang, S. Kudo, X. Bian, Z. Li, G. E. Karniadakis, *
+*                    A. Skillen                                              *
+*                                                                            *
+* This software is jointly licensed under the Apache License, Version 2.0    *
+* and the GNU General Public License version 3, you may use it according     *
+* to either.                                                                 *
+*                                                                            *
+* ** Apache License, version 2.0 **                                          *
+*                                                                            *
+* Licensed under the Apache License, Version 2.0 (the "License");            *
+* you may not use this file except in compliance with the License.           *
+* You may obtain a copy of the License at                                    *
+*                                                                            *
+* http://www.apache.org/licenses/LICENSE-2.0                                 *
+*                                                                            *
+* Unless required by applicable law or agreed to in writing, software        *
+* distributed under the License is distributed on an "AS IS" BASIS,          *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+* See the License for the specific language governing permissions and        *
+* limitations under the License.                                             *
+*                                                                            *
+* ** GNU General Public License, version 3 **                                *
+*                                                                            *
+* This program is free software: you can redistribute it and/or modify       *
+* it under the terms of the GNU General Public License as published by       *
+* the Free Software Foundation, either version 3 of the License, or          *
+* (at your option) any later version.                                        *
+*                                                                            *
+* This program is distributed in the hope that it will be useful,            *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+* GNU General Public License for more details.                               *
+*                                                                            *
+* You should have received a copy of the GNU General Public License          *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+*****************************************************************************/
 
-** Apache License, version 2.0 **
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-** File Details **
-
-Filename: sampler_rbf.h
-Created: Nov, 2018
-Author: A. Skillen
-Description: Spatial sampler using Gaussian Radial Basis Function interpolation.
-*/
+/**
+ * @file sampler_rbf.h
+ * @author A. SKillen
+ * @date November 2018
+ * @brief Spatial sampler using Gaussian Radial Basis Function interpolation.
+ */
 
 #ifndef MUI_SAMPLER_RBF_H_
 #define MUI_SAMPLER_RBF_H_
@@ -32,7 +54,7 @@ Description: Spatial sampler using Gaussian Radial Basis Function interpolation.
 
 namespace mui {
 
-template<typename O_TP, typename I_TP=O_TP, typename CONFIG=default_config>
+template<typename CONFIG=default_config, typename O_TP=default_config::REAL, typename I_TP=O_TP>
 class sampler_rbf 
 {
 public:
