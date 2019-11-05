e4bce37c0fc9fe6d2ef3ada693d7dce5a66cf339
Stephen Longshaw <stephen.longshaw@stfc.ac.uk>
2019-11-05 17:32:21 +0000

    Bug fix in spatial sampler typenames

--- spatial_samplers/sampler_mov_avg.h
+++ spatial_samplers/sampler_mov_avg.h
@@ -54,7 +54,7 @@
 
 namespace mui {
 
-template<typename CONFIG=default_config, typename O_TP=default_config::REAL, typename I_TP=O_TP>
+template<typename CONFIG=default_config, typename O_TP=typename CONFIG::REAL, typename I_TP=O_TP>
 class sampler_moving_average {
 public:
 	using OTYPE      = O_TP;
--- spatial_samplers/sampler_shepard_quintic.h
+++ spatial_samplers/sampler_shepard_quintic.h
@@ -55,7 +55,7 @@
 namespace mui
 {
 
-template<typename CONFIG=default_config, typename O_TP=default_config::REAL, typename I_TP=O_TP>
+template<typename CONFIG=default_config, typename O_TP=typename CONFIG::REAL, typename I_TP=O_TP>
 class sampler_shepard_quintic
 {
 public:
