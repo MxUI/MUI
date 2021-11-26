9e59c90a5ea30fee77a7b28accf65f01e5074395
Stephen Longshaw <stephen.longshaw@stfc.ac.uk>
2021-11-26 23:04:56 +0000

    Remove debug

--- uniface.h
+++ uniface.h
@@ -652,10 +652,6 @@ public:
 	  */
 	void barrier( time_type t1 ) {
 		auto start = std::chrono::system_clock::now();
-		for(size_t i=0; i<peers.size(); i++) {
-			if(peers[i].is_send_disabled())
-				std::cout << "MUI peer disabled: " << i << std::endl;
-		}
 		for(;;) {    // barrier must be thread-safe because it is called in fetch()
 			std::lock_guard<std::mutex> lock(mutex);
 			if( std::all_of(peers.begin(), peers.end(), [=](const peer_state& p) {
