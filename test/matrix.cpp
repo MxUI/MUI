f6e1111e6c6ae83762f48f25f70ad29baa82efed
Wendi-L <wendi.liu1988@icloud.com>
2023-01-28 16:13:06 +0000

    fix typo in matrix test code

--- test/matrix.cpp
+++ test/matrix.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	mui::linalg::sparse_matrix<int,double> a(3, 3);
+    mui::linalg::sparse_matrix<int,double> a(3, 3);
     a.set_value(0, 0, 1);
     a.set_value(0, 2, 2);
     a.set_value(1, 1, 3);
@@ -16,27 +16,27 @@ int main()
     std::cout << "Matrix A: " << std::endl;
     a.print();
 
-	std::cout << "Matrix B: " << std::endl;
-	b.print();
-	
-	mui::linalg::sparse_matrix<int,double> c = a + b;
+    std::cout << "Matrix B: " << std::endl;
+    b.print();
+
+    mui::linalg::sparse_matrix<int,double> c = a + b;
     std::cout << "Addition of matrices (A+B): " << std::endl;
     c.print();
 
-	c.set_zero();
-	c = a - b;
-	std::cout << "Subtraction of matrices (A-B): " << std::endl;
-	c.print();
+    c.set_zero();
+    c = a - b;
+    std::cout << "Subtraction of matrices (A-B): " << std::endl;
+    c.print();
 
-	c.set_zero();
-	c = a * b;
-	std::cout << "Multiplication of matrices (A*B): " << std::endl;
-	c.print();
+    c.set_zero();
+    c = a * b;
+    std::cout << "Multiplication of matrices (A*B): " << std::endl;
+    c.print();
 
-	c.set_zero();
-	c = a.transpose();
-	std::cout << "Transpose of A matrix (A^T): " << std::endl;
-	c.print();
+    c.set_zero();
+    c = a.transpose();
+    std::cout << "Transpose of A matrix (A^T): " << std::endl;
+    c.print();
 
     return 0;
    }
