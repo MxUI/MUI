23fe4823ea97fd903ac41ca46224c59d6e7af627
Wendi-L <wendi.liu1988@icloud.com>
2023-02-01 21:36:04 +0000

    Add comment on virtual apply

--- linalg/preconditioner_base.h
+++ linalg/preconditioner_base.h
@@ -54,6 +54,7 @@ namespace linalg {
 template<typename ITYPE, typename VTYPE>
 class preconditioner {
     public:
+        // Virtual function on preconditioner apply
         virtual sparse_matrix<ITYPE,VTYPE> apply(const sparse_matrix<ITYPE,VTYPE> &) = 0;
 };
 
