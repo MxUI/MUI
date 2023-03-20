9e43e682d97d78e5a90d85bff17f884d4d7ad0df
Wendi-L <wendi.liu1988@icloud.com>
2023-03-20 22:24:53 +0000

    Updare AlgorithmAitken & AlgorithmFixedRelaxation in python wrapper

--- wrappers/Python/mui4py/algorithms.py
+++ wrappers/Python/mui4py/algorithms.py
@@ -56,51 +56,59 @@ def algorithm_signature(self):
 
 # Algorithms
 class Algorithm(CppClass):
-    def __init__(self, args=(), kwargs={}):
-        # Empty config to not trigger error in default config.
-        super(Algorithm, self).__init__(Config(), args, kwargs)
+    def __init__(self, config=None, args=(), kwargs={}):
+        if config is not None:
+            super(Algorithm, self).__init__(config, args, kwargs)
+            self.configure(self.config)
+        else:
+            # Empty config to not trigger error in default config.
+            super(Algorithm, self).__init__(Config(), args, kwargs)
 
 Algorithm.fetch_signature = algorithm_signature
 
 class AlgorithmFixedRelaxation(Algorithm):
-    def __init__(self, under_relaxation_factor=None, pts_value_init=None):
+    def __init__(self, under_relaxation_factor=None, pts_value_init=None, config=None):
+        if config is None:
+            self.config=Config()
         if pts_value_init is not None:
-            super(AlgorithmFixedRelaxation, self).__init__(args=(under_relaxation_factor, pts_value_init))
+            super(AlgorithmFixedRelaxation, self).__init__(config, args=(under_relaxation_factor, pts_value_init))
         else:
             if under_relaxation_factor is not None:
-                super(AlgorithmFixedRelaxation, self).__init__(args=(under_relaxation_factor,))
+                super(AlgorithmFixedRelaxation, self).__init__(config, args=(under_relaxation_factor,))
             else:
                 super(AlgorithmFixedRelaxation, self).__init__()
         self._ALLOWED_IO_TYPES = [INT, INT32, INT64, UINT, UINT32, UINT64, FLOAT, FLOAT32, FLOAT64]
 
 class AlgorithmAitken(Algorithm):
-    def __init__(self, under_relaxation_factor=None, under_relaxation_factor_max=None, local_comm=None, pts_vlu_init=None, res_l2_norm_nm1=None):
+    def __init__(self, under_relaxation_factor=None, under_relaxation_factor_max=None, local_comm=None, pts_vlu_init=None, res_l2_norm_nm1=None, config=None):
+        if config is None:
+            self.config=Config()
         if res_l2_norm_nm1 is not None:
-            super(AlgorithmAitken, self).__init__(args=(under_relaxation_factor, under_relaxation_factor_max, local_comm, pts_vlu_init, res_l2_norm_nm1))
+            super(AlgorithmAitken, self).__init__(config, args=(under_relaxation_factor, under_relaxation_factor_max, local_comm, pts_vlu_init, res_l2_norm_nm1))
         else:
             if pts_vlu_init is not None:
-                super(AlgorithmAitken, self).__init__(args=(under_relaxation_factor, under_relaxation_factor_max, local_comm, pts_vlu_init, 0.0))
+                super(AlgorithmAitken, self).__init__(config, args=(under_relaxation_factor, under_relaxation_factor_max, local_comm, pts_vlu_init, 0.0))
             else:
                 if local_comm is not None:
-                    super(AlgorithmAitken, self).__init__(args=(under_relaxation_factor, under_relaxation_factor_max, local_comm, [], 0.0))
+                    super(AlgorithmAitken, self).__init__(config, args=(under_relaxation_factor, under_relaxation_factor_max, local_comm, [], 0.0))
                 else:
                     if under_relaxation_factor_max is not None:
-                        super(AlgorithmAitken, self).__init__(args=(under_relaxation_factor, under_relaxation_factor_max, None, [], 0.0))
+                        super(AlgorithmAitken, self).__init__(config, args=(under_relaxation_factor, under_relaxation_factor_max, None, [], 0.0))
                     else:
                         if under_relaxation_factor is not None:
-                            super(AlgorithmAitken, self).__init__(args=(under_relaxation_factor, 1.0, None, [], 0.0))
+                            super(AlgorithmAitken, self).__init__(config, args=(under_relaxation_factor, 1.0, None, [], 0.0))
                         else:
-                            super(AlgorithmAitken, self).__init__(args=(1.0, 1.0, None, [], 0.0))
+                            super(AlgorithmAitken, self).__init__(config, args=(1.0, 1.0, None, [], 0.0))
         self._ALLOWED_IO_TYPES = [INT, INT32, INT64, UINT, UINT32, UINT64, FLOAT, FLOAT32, FLOAT64]
 
     def get_under_relaxation_factor(self, t1, t2=None):
         if t2 is not None:
-            self.raw.get_under_relaxation_factor(t1, t2)
+            return self.raw.get_under_relaxation_factor(t1, t2)
         else:
-            self.raw.get_under_relaxation_factor(t1)
+            return self.raw.get_under_relaxation_factor(t1)
 
     def get_residual_L2_Norm(self, t1, t2=None):
         if t2 is not None:
-            self.raw.get_residual_L2_Norm(t1, t2)
+            return self.raw.get_residual_L2_Norm(t1, t2)
         else:
-            self.raw.get_residual_L2_Norm(t1)
+            return self.raw.get_residual_L2_Norm(t1)
