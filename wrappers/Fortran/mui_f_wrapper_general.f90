e57182eea3698b4fe3e47782671ab85cd3c930fa
Wendi-L <wendi.liu1988@icloud.com>
2023-03-24 12:02:39 +0000

    Add get_size and get_rank functions to fortran wrapper

--- wrappers/Fortran/mui_f_wrapper_general.f90
+++ wrappers/Fortran/mui_f_wrapper_general.f90
@@ -68,6 +68,20 @@ module mui_general_f
       character(kind=c_char), intent(in), dimension(argc) :: argv(*)
     end subroutine mui_mpi_split_by_app_threaded_f
 
+    !Function to get MPI size
+    subroutine mui_mpi_get_size_f(communicator,local_size) bind(C)
+      import :: c_ptr,c_int
+      type(c_ptr), intent(out), target :: communicator(*)
+      integer(kind=c_int), intent(in) :: local_size
+    end subroutine mui_mpi_get_size_f
+
+    !Function to get MPI rank
+    subroutine mui_mpi_get_rank_f(communicator,local_rank) bind(C)
+      import :: c_ptr,c_int
+      type(c_ptr), intent(out), target :: communicator(*)
+      integer(kind=c_int), intent(in) :: local_rank
+    end subroutine mui_mpi_get_rank_f
+
   end interface 
 
 end module mui_general_f
